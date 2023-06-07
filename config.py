d0ba5c664558bd00e88d9d67b37cf50e8f2fec6b
Cutey02 <116083642+Cutey02@users.noreply.github.com>
2023-06-07 17:15:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,8 +25,8 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Cutey02/dy")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/nanbargalisai")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/nanbargalaarangam")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/remo_editz")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/TamilStatusForGirl")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
