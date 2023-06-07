6d94863fe6531093e3adca293c03d679a6ed03d4
Cutey02 <116083642+Cutey02@users.noreply.github.com>
2023-06-07 14:26:14 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -1,1 +1,56 @@
+from typing import Union
 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+import config
+
+
+def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="ʜᴇʟᴩ",
+                callback_data="settings_back_helper",
+            ),
+            InlineKeyboardButton(
+                text="sᴇᴛᴛɪɴɢs", callback_data="settings_helper"
+            ),
+        ],
+     ]
+    return buttons
+
+
+def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="ʜᴇʟᴩ", callback_data="settings_back_helper"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="❣ sᴛᴀᴛᴜs ❣", url=config.SUPPORT_GROUP
+            ),
+            InlineKeyboardButton(
+                text="🥀☊ Assɪsᴛᴀɴᴛ ☊🥀", user_id=OWNER
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="✪ ʙᴀᴅ ʙᴏʏ ✪", url="https://t.me/badboybiografia"
+            )
+        ],
+     ]
+    return buttons
